9899c8f09714ba3a3a5731da0abbaa356797d71f
erfan-taraghi <taraghikonerfan@gmail.com>
2022-06-02 10:10:37 +0430

    slider change bg color

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imageSlider(anything){
+    document.querySelector('.bmw').src = anything
+}
+
+function changeBgColor(anything){
+    const section = document.querySelector('.section');
+    section.style.background = anything
+}
